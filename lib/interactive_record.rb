94a9fe8ce42eba29bb943f435156c448c915868b
David Moore <hey@iamdavidmoore.com>
2018-07-05 23:43:12 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,5 +26,13 @@ class InteractiveRecord
     end
   end
 
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def column_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
 
 end
